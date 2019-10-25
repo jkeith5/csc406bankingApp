ed7b57099937eaeed7da9fa4917ea5cc3cd297e2
trenten93 <35821142+trenten93@users.noreply.github.com>
2019-10-25 10:23:37 -0500

    Added a CustomerAccount Class to hold objects for arraylist. It's empty for now.

---
+++ src/BankingApp/CustomerAccount.java
@@ -0,0 +1,10 @@
+package BankingApp;
+
+public class CustomerAccount {
+
+
+    // This will hold all data for customer account in the ArrayList of CustomerAccouts Objects
+
+
+    // will have info on all accounts savings cds Checking Credit Card all all relevant info
+}
