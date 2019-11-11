0285dc8ab7fe91b0697aa92bfa2b6b758a136ab5
trenten93 <35821142+trenten93@users.noreply.github.com>
2019-11-11 13:50:17 -0600

    added a FinanceDriver to hold methods to complete financial transactions on the customer account objects. this is to prevent over clutter of the Controller or DataEntryDriver classes.

---
+++ src/BankingApp/FinanceDriver.java
@@ -0,0 +1,12 @@
+package BankingApp;
+
+public class FinanceDriver {
+    // this will have all the methods that conduct financial transactions on the Customer Account objects.
+    // most, if not all, methods here will be called from the Controller class, and will hold things like
+    // transfer debit credit methods.
+
+
+
+
+
+}
